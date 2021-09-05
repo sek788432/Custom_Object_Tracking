ee455984cb11362030601150d0138258a4544a0b
Jeff <48002319+sek788432@users.noreply.github.com>
2021-09-05 17:03:52 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='objectTracking',
+      version='1.0',
+      description='Object Tracking',
+      author='Jeff Lo',
+      author_email='jefflo.861104@gmail.com',
+      url='https://github.com/sek788432/Waymo-Object-Tracking',
+      packages=['objectTracking_'],
+     )
